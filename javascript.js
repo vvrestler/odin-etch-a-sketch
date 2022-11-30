69049b9522438a36a12bedd98b6f9cb6db26ae7e
gary <110794817+vvrestler@users.noreply.github.com>
2022-11-30 16:11:45 -0500

    Redraw grid when size is specified

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ const container = document.querySelector('#container');
 
 for(i=0; i<16; i++) {
     for(j=0;j<16;j++) {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'box');
-        div.addEventListener('mouseenter', () => {div.style.backgroundColor = 'orange'} )
-        container.appendChild(div);
+        const box = document.createElement('div');
+        box.setAttribute('class', 'box');
+        box.addEventListener('mouseenter', () => {box.style.backgroundColor = 'orange'} )
+        container.appendChild(box);
     }
 }
 
@@ -13,16 +13,22 @@ const setGrid = function() {
     let num = 0;
     num = prompt('How many rows and columns do you want? Pick from 1 to 64.');
 
-   /* let gridItems = container.querySelectorAll('.box');
-    gridItems.forEach(() => container.removeChild(div))
+    let gridItems = container.querySelectorAll('.box');
+    gridItems.forEach( () => {
+        const div = container.querySelector('div');
+        container.removeChild(div);
+    });  
+    
+    //console.log(gridItems);
 
     for(i=0; i<num; i++) {
         for(j=0;j<num;j++) {
-            div.setAttribute('class', 'box');
-            div.addEventListener('mouseenter', () => {div.style.backgroundColor = 'orange'} )
-            container.appendChild(div);
+            const box = document.createElement('div');
+            box.setAttribute('class', 'box');
+            box.addEventListener('mouseenter', () => {box.style.backgroundColor = 'orange'} )
+            container.appendChild(box);
         }
-    }   */
+    }   
 }
 
 const gridSize = document.querySelector('#gridAmount');
