42207db46b27373a2a9f32e48d9abeba52f0277e
gary <110794817+vvrestler@users.noreply.github.com>
2022-11-30 01:00:31 -0500

    Change grid boxes color on hover

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ for(i=0; i<16; i++) {
     for(j=0;j<16;j++) {
         const div = document.createElement('div');
         div.setAttribute('class', 'box');
+        div.addEventListener('mouseenter', () => {div.style.backgroundColor = 'orange'} )
         container.appendChild(div);
+
     }
 }
