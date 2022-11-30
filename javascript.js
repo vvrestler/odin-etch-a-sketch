4d5504d0fe06296767bad277a05de405bc62076a
gary <110794817+vvrestler@users.noreply.github.com>
2022-11-30 15:56:07 -0500

    Add button asking for grid size

--- javascript.js
+++ javascript.js
@@ -6,6 +6,24 @@ for(i=0; i<16; i++) {
         div.setAttribute('class', 'box');
         div.addEventListener('mouseenter', () => {div.style.backgroundColor = 'orange'} )
         container.appendChild(div);
-
     }
 }
+
+const setGrid = function() {
+    let num = 0;
+    num = prompt('How many rows and columns do you want? Pick from 1 to 64.');
+
+   /* let gridItems = container.querySelectorAll('.box');
+    gridItems.forEach(() => container.removeChild(div))
+
+    for(i=0; i<num; i++) {
+        for(j=0;j<num;j++) {
+            div.setAttribute('class', 'box');
+            div.addEventListener('mouseenter', () => {div.style.backgroundColor = 'orange'} )
+            container.appendChild(div);
+        }
+    }   */
+}
+
+const gridSize = document.querySelector('#gridAmount');
+gridSize.addEventListener('click', setGrid);
