82045c988c95813d4e14291c3fd49e0aea02c52b
gary <110794817+vvrestler@users.noreply.github.com>
2022-11-30 00:39:07 -0500

    Change grid to 16x16

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('#container');
 
-for(i=0; i<8; i++) {
-    for(j=0;j<8;j++) {
+for(i=0; i<16; i++) {
+    for(j=0;j<16;j++) {
         const div = document.createElement('div');
         div.setAttribute('class', 'box');
         container.appendChild(div);
