e997514eada7e94079aa2842b9edd7da39bcd28d
gary <110794817+vvrestler@users.noreply.github.com>
2022-12-01 18:47:06 -0500

    Resize grid after it is redrawn

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 
-for(i=0; i<16; i++) {
+for(i=0; i<16; i++) {   //create initial grid
     for(j=0;j<16;j++) {
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
@@ -19,17 +19,23 @@ const setGrid = function() {
         container.removeChild(div);
     });  
     
-    //console.log(gridItems);
-
-    for(i=0; i < gridSize; i++) {
-        for(j=0; j < gridSize;j++) {
+    for(let i=0; i < gridSize; i++) {       //redraw grid
+        for(let j=0; j < gridSize;j++) {
             const box = document.createElement('div');
             box.setAttribute('class', 'box');
-            box.addEventListener('mouseenter', () => {box.style.backgroundColor = 'orange'} )
+            box.addEventListener('mouseenter', () => {box.style.backgroundColor = 'orange';})
             container.appendChild(box);
         }
     }   
+
+    let boxSize = 320 / gridSize;   //resize the redrawn grid
+    const boxesToResize = container.getElementsByClassName('box');
+    for(let i=0; i<boxesToResize.length; i++) {
+        container.getElementsByClassName('box')[i].style.width = boxSize + 'px';
+        container.getElementsByClassName('box')[i].style.height = boxSize + 'px';
+    }
 }
 
 const gridSize = document.querySelector('#gridAmount');
 gridSize.addEventListener('click', setGrid);
+//box style -> size of grid divided by number of rows= box size
